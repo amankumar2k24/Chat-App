1ae857be1cd0530848dcba69f88a26c0a95d6566
Aman Kumar <aman.kumar2k15@gmail.com>
2024-09-29 15:18:32 +0530

    fixed origin

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const PORT = process.env.PORT || 6000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://aman-chat-app.vercel.app",
-    // origin: allowedOrigins,
+    // origin: "http://localhost:3000",
+    origin: allowedOrigins,
     credentials: true,
   })
 );
