9073a94f7c8e6d2c94e9c5053245692e8b395599
Aman Kumar <aman.kumar2k15@gmail.com>
2024-09-29 22:21:12 +0530

    Update allowedOrigins.js

--- utils/allowedOrigins.js
+++ utils/allowedOrigins.js
@@ -1,6 +1,5 @@
 export const allowedOrigins = [
   "http://localhost:3000",
-  "https://aman-chat-app.vercel.app/login",
   "https://aman-chat-app.vercel.app",
-  "https://aman-chat-app.vercel.app/",
 ];
+  
